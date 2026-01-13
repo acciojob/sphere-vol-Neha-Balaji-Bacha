cd6285970af29c7c42efb4db02c8c26881022cf9
Neha Bacha <neha170603@gmail.com>
2026-01-13 11:33:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const radius = document.querySelector("#radius");
+const volume = documnet.querSelector("#volume");
+let result;
 function volume_sphere() {
-    //Write your code here
-  
+    let r = radius.value;
+	if(r >= 0 && (!isNaN)){
+		result = Math.round((Math.floor(4/3)) / 3.14 * Math.pow(r,3));
+	}
+   return result;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
