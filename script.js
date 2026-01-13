3bf9c116f07a709d3606c35ce3a428604e8631c5
Neha Bacha <neha170603@gmail.com>
2026-01-13 11:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const radius = document.querySelector("#radius");
-const volume = documnet.querySelector("#volume");
+const volume = document.querySelector("#volume");
 let result;
 function volume_sphere() {
     let r = Number(radius.value);
-	if(r < 0 && isNaN(r)){
-		result = NaN;
+	if(r < 0 || isNaN(r)){
+		result = "NaN";
 	}
     else{
-		result = ((4/3) * Math.PI * Math.pow(r,3).toFixed(4));
+		result = ((4/3) * Math.PI * Math.pow(r,3)).toFixed(4);
 	}
 	volume.value = result;
 } 
